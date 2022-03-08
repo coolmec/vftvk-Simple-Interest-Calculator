a17597cb869c4a29984734bb96ff08e93744416c
Jaffar MOUKAILA <jmoukaila@yahoo.fr>
2022-03-08 17:12:01 +0000

    Modification of the program following requestor requirements

--- script.js
+++ script.js
@@ -7,15 +7,19 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     if (principal <= 0) {
         alert("Enter a positive number");
-        principal.focus();
+        document.getElementById("principal").focus();
+        return false;
     }    
     document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+
-    "\<mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<mark\>\<br\>You will receive an amount of \<mark\>"+
-    amount+"\<mark\>,\<br\>in the year \<mark\>"+year+"\<mark\>\<br\>";
+    "\</mark\>,\<br\>at an interest rate of \<mark\>"+rate+
+    "%\</mark\>\<br\>You will receive an amount of \<mark\>"+interest+
+    "\</mark\>,\<br\>in the year \<mark\>"+year+"\</mark\>\<br\>";
+    return true;
 }
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval+"%";
+    return true;
 }
         
